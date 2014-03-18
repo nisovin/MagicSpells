24ea33ffc52da744905d267468b59fb36a6c8bbf
nisovin <nisovin@gmail.com>
2014-03-18 12:03:17 -0600

    Added factions and towny

---
+++ src_factions/com/nisovin/magicspells/factions/MagicSpellsFactions.java
@@ -0,0 +1,49 @@
+package com.nisovin.magicspells.factions;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.massivecraft.factions.FFlag;
+import com.massivecraft.factions.Rel;
+import com.massivecraft.factions.entity.BoardColls;
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.UPlayer;
+import com.massivecraft.mcore.ps.PS;
+import com.nisovin.magicspells.events.SpellTargetEvent;
+
+public class MagicSpellsFactions extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler
+	public void onSpellTarget(SpellTargetEvent event) {
+		if (!(event.getTarget() instanceof Player)) return;
+		
+		boolean beneficial = event.getSpell().isBeneficial();
+		UPlayer caster = UPlayer.get(event.getCaster());
+		UPlayer target = UPlayer.get(event.getTarget());
+		
+		Rel rel = caster.getRelationTo(target);
+		if (rel.isFriend() && !beneficial) {
+			event.setCancelled(true);
+		} else if (!rel.isFriend() && beneficial) {
+			event.setCancelled(true);
+		}
+		
+		Faction faction = BoardColls.get().getFactionAt(PS.valueOf(event.getCaster().getLocation()));
+		if (faction != null && !faction.getFlag(FFlag.PVP)) {
+			event.setCancelled(true);
+		}
+		faction = BoardColls.get().getFactionAt(PS.valueOf(event.getTarget().getLocation()));
+		if (faction != null && !faction.getFlag(FFlag.PVP)) {
+			event.setCancelled(true);
+		}
+	}
+	
+}
---
+++ src_towny/com/nisovin/magicspells/towny/MagicSpellsTowny.java
@@ -0,0 +1,93 @@
+package com.nisovin.magicspells.towny;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.Spell;
+import com.nisovin.magicspells.events.SpellCastEvent;
+import com.nisovin.magicspells.events.SpellTargetEvent;
+import com.nisovin.magicspells.spells.TargetedSpell;
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Coord;
+import com.palmergames.bukkit.towny.object.TownyUniverse;
+import com.palmergames.bukkit.towny.object.TownyWorld;
+import com.palmergames.bukkit.towny.utils.CombatUtil;
+
+public class MagicSpellsTowny extends JavaPlugin implements Listener {
+	
+	private Set<Spell> disallowedInTowns = new HashSet<Spell>();
+	private Towny towny;
+	
+	@Override
+	public void onEnable() {
+		File file = new File(getDataFolder(), "config.yml");
+		if (!file.exists()) {
+			saveDefaultConfig();
+		}
+		Configuration config = getConfig();
+		if (config.contains("disallowed-in-towns")) {
+			List<String> list = config.getStringList("disallowed-in-towns");
+			for (String s : list) {
+				Spell spell = MagicSpells.getSpellByInternalName(s);
+				if (spell == null) {
+					spell = MagicSpells.getSpellByInGameName(s);
+				}
+				if (spell != null) {
+					disallowedInTowns.add(spell);
+				} else {
+					getLogger().warning("Could not find spell " + s);
+				}
+			}
+		}
+		
+		Plugin townyPlugin = getServer().getPluginManager().getPlugin("Towny");
+		if (townyPlugin != null) {
+			towny = (Towny)townyPlugin;
+			getServer().getPluginManager().registerEvents(this, this);
+		} else {
+			getLogger().severe("Failed to find Towny");
+			this.setEnabled(false);
+		}
+	}
+	
+	@EventHandler(ignoreCancelled=true)
+	public void onSpellTarget(SpellTargetEvent event) {
+		boolean friendlySpell = false;
+		if (event.getSpell() instanceof TargetedSpell && ((TargetedSpell)event.getSpell()).isBeneficial()) {
+			friendlySpell = true;
+		}
+		if (!friendlySpell && CombatUtil.preventDamageCall(towny, event.getCaster(), event.getTarget())) {
+			event.setCancelled(true);
+		} else if (friendlySpell && event.getTarget() instanceof Player && !CombatUtil.isAlly(event.getCaster().getName(), ((Player)event.getTarget()).getName())) {
+			event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler(ignoreCancelled=true)
+	public void onSpellCast(SpellCastEvent event) {
+		if (disallowedInTowns.contains(event.getSpell())) {
+			try {
+				TownyWorld world = TownyUniverse.getDataSource().getWorld(event.getCaster().getWorld().getName());
+				if (world != null && world.isUsingTowny()) {
+					Coord coord = Coord.parseCoord(event.getCaster());
+					if (world.getTownBlock(coord) != null) {
+						event.setCancelled(true);
+					}
+				}
+			} catch (NotRegisteredException e) {
+			}
+		}
+	}
+	
+}
