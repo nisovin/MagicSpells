05376f6b264d2d7e63579e1c05ee49103b45520c
nisovin <github@nisovin.com>
2014-05-21 12:52:34 -0600

    Resist spell changes

--- src/com/nisovin/magicspells/spells/buff/ResistSpell.java
+++ src/com/nisovin/magicspells/spells/buff/ResistSpell.java
@@ -1,10 +1,13 @@
 package com.nisovin.magicspells.spells.buff;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import com.nisovin.magicspells.events.SpellTargetEvent;
 import com.nisovin.magicspells.spells.BuffSpell;
@@ -13,16 +16,29 @@ import com.nisovin.magicspells.util.MagicConfig;
 
 public class ResistSpell extends BuffSpell {
 
-	String spellDamageType;
-	float powerMultiplier;
+	List<String> spellDamageTypes;
+	List<DamageCause> normalDamageTypes;
+	float multiplier;	
 	
 	Map<String, Float> buffed = new HashMap<String, Float>();
 	
 	public ResistSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 		
-		spellDamageType = getConfigString("spell-damage-type", "");
-		powerMultiplier = getConfigFloat("power-multiplier", 0.5F);
+		spellDamageTypes = getConfigStringList("spell-damage-types", null);
+		List<String> list = getConfigStringList("normal-damage-types", null);
+		multiplier = getConfigFloat("multiplier", 0.5F);
+		
+		if (list != null) {
+			for (String s : list) {
+				for (DamageCause cause : DamageCause.values()) {
+					if (cause.name().equalsIgnoreCase(s)) {
+						normalDamageTypes.add(cause);
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	@Override
@@ -31,14 +47,21 @@ public class ResistSpell extends BuffSpell {
 		return true;
 	}
 	
-	@EventHandler
+	@EventHandler(ignoreCancelled = true)
 	public void onSpellTarget(SpellTargetEvent event) {
-		if (event.getSpell() instanceof SpellDamageSpell && isActive(event.getCaster())) {
+		if (spellDamageTypes != null && event.getSpell() instanceof SpellDamageSpell && isActive(event.getCaster())) {
 			SpellDamageSpell spell = (SpellDamageSpell)event.getSpell();
-			if (spell.getSpellDamageType() != null && spell.getSpellDamageType().equals(spellDamageType)) {
-				float power = (1 / buffed.get(event.getCaster().getName())) * powerMultiplier;
+			if (spell.getSpellDamageType() != null && spellDamageTypes.contains(spell.getSpellDamageType())) {
+				float power = (1 / buffed.get(event.getCaster().getName())) * multiplier;
 				event.increasePower(power);
 			}
+		}
+	}
+	
+	@EventHandler(ignoreCancelled = true)
+	public void onEntityDamage(EntityDamageEvent event) {
+		if (normalDamageTypes != null && normalDamageTypes.contains(event.getCause()) && event.getEntity() instanceof Player && buffed.containsKey(((Player)event.getEntity()).getName())) {
+			event.setDamage(event.getDamage() * multiplier);
 		}
 	}
 
