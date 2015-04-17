547b839d0a2eddd0b8d22045310bfb091b74e41f
nisovin <nisovin@gmail.com>
2015-04-17 15:00:36 -0600

    Added missing import

--- src/com/nisovin/magicspells/volatilecode/VolatileCodeEnabled_1_7_Spigot.java
+++ src/com/nisovin/magicspells/volatilecode/VolatileCodeEnabled_1_7_Spigot.java
@@ -3,6 +3,7 @@ package com.nisovin.magicspells.volatilecode;
 import java.io.File;
 import java.io.FileWriter;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
