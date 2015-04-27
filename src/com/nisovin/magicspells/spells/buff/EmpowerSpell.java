7c837444b176af3d356b91e159fad6f6dca53c25
nisovin <nisovin@gmail.com>
2015-04-27 16:27:25 -0600

    Added max-power-multiplier to EmpowerSpell
    The power can now stack on repeated casts of this spell

--- src/com/nisovin/magicspells/spells/buff/EmpowerSpell.java
+++ src/com/nisovin/magicspells/spells/buff/EmpowerSpell.java
@@ -16,41 +16,55 @@ import com.nisovin.magicspells.util.MagicConfig;
 public class EmpowerSpell extends BuffSpell {
 
 	private float extraPower;
+	private float maxPower;
 	private Set<String> spells;
 	
-	private HashMap<Player, Float> empowered;
+	private HashMap<String, Float> empowered;
 	
 	public EmpowerSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 		
 		extraPower = getConfigFloat("power-multiplier", 1.5F);
+		maxPower = getConfigFloat("max-power-multiplier", 1.5F);
 		List<String> list = getConfigStringList("spells", null);
 		if (list != null && list.size() > 0) {
 			spells = new HashSet<String>(list);
 		}
 		
-		empowered = new HashMap<Player, Float>();
+		empowered = new HashMap<String, Float>();
 	}
 
 	@Override
 	public boolean castBuff(Player player, float power, String[] args) {
 		float p = power * extraPower;
-		empowered.put(player, p);
+		if (p > maxPower) p = maxPower;
+		empowered.put(player.getName(), p);
+		return true;
+	}
+	
+	@Override
+	public boolean recastBuff(Player player, float power, String[] args) {
+		if (maxPower > extraPower) {
+			float p = empowered.get(player.getName());
+			p += power * extraPower;
+			if (p > maxPower) p = maxPower;
+			empowered.put(player.getName(), p);
+		}
 		return true;
 	}
 
 	@EventHandler(priority=EventPriority.HIGHEST)
 	public void onSpellCast(SpellCastEvent event) {
 		Player player = event.getCaster();
-		if (empowered.containsKey(player) && (spells == null || spells.contains(event.getSpell().getInternalName()))) {
-			event.increasePower(empowered.get(player));
+		if (empowered.containsKey(player.getName()) && (spells == null || spells.contains(event.getSpell().getInternalName()))) {
+			event.increasePower(empowered.get(player.getName()));
 			addUseAndChargeCost(player);
 		}
 	}
 	
 	@Override
 	public void turnOffBuff(Player player) {
-		empowered.remove(player);
+		empowered.remove(player.getName());
 	}
 
 	@Override
@@ -60,7 +74,7 @@ public class EmpowerSpell extends BuffSpell {
 
 	@Override
 	public boolean isActive(Player player) {
-		return empowered.containsKey(player);
+		return empowered.containsKey(player.getName());
 	}
 
 }
